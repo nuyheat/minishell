30053efd2f7472619b43db9560953337cb63a7e0
Taehyun Kim <taehkim2@c3r3s7.42seoul.kr>
2023-11-22 14:51:53 +0900

    리팩토링 필요 / pars_tokenize 분할

--- parse/parse.c
+++ parse/parse.c
@@ -6,13 +6,13 @@
 /*   By: taehkim2 <taehkim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 12:24:31 by taehkim2          #+#    #+#             */
-/*   Updated: 2023/11/22 13:47:24 by taehkim2         ###   ########.fr       */
+/*   Updated: 2023/11/22 14:50:00 by taehkim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	parse_tokenize(t_list **list, char **buf, char now_char)
+int	tokenize_rules(t_list **list, char **buf, char now_char)
 {
 	if (tokenize_operator_double(buf, now_char))
 		return (NEXT);
@@ -38,7 +38,7 @@ int	parse_tokenize(t_list **list, char **buf, char now_char)
 	return (NEXT);
 }
 
-void	parse(t_list **list, char *line)
+void	parse_tokenize(t_list **list, char *line)
 {
 	int		idx;
 	char	*buf;
@@ -49,10 +49,18 @@ void	parse(t_list **list, char *line)
 		error_end("calloc failed");
 	while (line[idx] != '\0')
 	{
-		if (parse_tokenize(list, &buf, line[idx]) == END)
+		if (tokenize_rules(list, &buf, line[idx]) == END)
 			break ;
 		idx++;
 	}
 	token_delimited(list, &buf);
 	free(buf);
+}
+
+void	parse(t_list **list, char *line)
+{
+	t_list	*list_head;
+
+	list_head = (*list);
+	parse_tokenize(*list, line);
 }
