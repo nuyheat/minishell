94c807367102906c373479fbbe1fdc83b87a629c
Taehyun Kim <taehkim2@c3r2s7.42seoul.kr>
2023-12-12 17:52:35 +0900

    merge 완료

--- builtins/built_exit.c
+++ builtins/built_exit.c
@@ -6,11 +6,7 @@
 /*   By: taehkim2 <taehkim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 16:24:56 by sihlee            #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2023/12/12 12:04:45 by sihlee           ###   ########.fr       */
-=======
-/*   Updated: 2023/12/12 17:01:40 by taehkim2         ###   ########.fr       */
->>>>>>> 3171dfb6db91a062e0e4a4d70f3b4b2ec3ffa799
+/*   Updated: 2023/12/12 17:52:07 by taehkim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,25 +76,14 @@ void	my_exit(char **argv)
 
 	argv_idx = 2;
 	exit_num = 0;
-<<<<<<< HEAD
 	printf("exit\n");
 	if (argv == NULL || argv[1] == NULL)
-=======
-	if (argv == NULL || argv[1] == NULL)
-	{
-		printf("\b\bexit\n");
->>>>>>> 3171dfb6db91a062e0e4a4d70f3b4b2ec3ffa799
 		exit(exit_num); // 이거 이전 $? 리턴값으로 exit_num을 넘겨줘야함
-	}
 	else if (argv[2] == NULL)
 	{
-		printf("exit\n");
 		exit_num = check_exit_num(argv[1]);
 		exit(exit_num);
 	}
 	else
-	{
-		printf("exit\n");
 		write(2, "minishell: exit: too many arguments\n", 36);
-	}
 }
