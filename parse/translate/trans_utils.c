ca8a290717a83cfedcf5c192534765eb1f5ce4cd
Taehyun Kim <taehkim2@c5r2s1.42seoul.kr>
2024-01-15 14:03:17 +0900

    split_token_init 수정 완료

--- parse/translate/trans_utils.c
+++ parse/translate/trans_utils.c
@@ -6,7 +6,7 @@
 /*   By: taehkim2 <taehkim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 17:08:33 by taehkim2          #+#    #+#             */
-/*   Updated: 2023/12/16 14:56:18 by taehkim2         ###   ########.fr       */
+/*   Updated: 2024/01/15 14:02:44 by taehkim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,33 +22,11 @@ int	row_cnt(char **str)
 	return (idx);
 }
 
-int	split_cnt(char *token)
-{
-	int	idx;
-	int	dollar_cnt;
-	int	another_cnt;
-
-	idx = 0;
-	dollar_cnt = 0;
-	another_cnt = 0;
-	while (token[idx] != '\0')
-	{
-		if (token[idx] == '$')
-			dollar_cnt++;
-		if (token[idx] == '\"' || token[idx] == '/' \
-			|| token[idx] == '=' || token[idx] == '?' \
-			|| token[idx] == '\'' || token[idx] == ' ')
-			another_cnt++;
-		idx++;
-	}
-	return (dollar_cnt * 3 + another_cnt + 1);
-}
-
 char	**splited_token_init(char *token)
 {
 	char	**splited_token;
 
-	splited_token = malloc(sizeof(char *) * (split_cnt(token) + 1));
+	splited_token = malloc(sizeof(char *) * (ft_strlen(token) + 1));
 	if (splited_token == NULL)
 		error_end("malloc failed");
 	splited_token[0] = NULL;
