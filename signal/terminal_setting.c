ca84f92307b93c71d48c2da552f9a6d45a5b1c70
nuyheat <taehkim2@student.42seoul.kr>
2026-06-18 15:32:31 +0900

    chore: terminal_setting.c 파일 주석 추가

--- signal/terminal_setting.c
+++ signal/terminal_setting.c
@@ -12,13 +12,15 @@
 
 #include "minishell.h"
 
+// terminal 의 ctrl echo 설정을 끄는 함수
 void	ctrl_echo_off(struct termios *terminal)
 {
-	tcgetattr(STDIN_FILENO, terminal);
-	terminal->c_lflag &= ~(ECHOCTL);
-	tcsetattr(STDIN_FILENO, TCSANOW, terminal);
+	tcgetattr(STDIN_FILENO, terminal); // 현재 terminal의 속성정보를 받아옴
+	terminal->c_lflag &= ~(ECHOCTL); // 제어문자(/C, /D 등)의 출력을 제거
+	tcsetattr(STDIN_FILENO, TCSANOW, terminal); // 터미널 설정에 즉시 반영(TCSANOW -> terminal control set attribute now)
 }
 
+// terminal 의 ctrl echo 설정을 켜는 함수
 void	ctrl_echo_on(struct termios *terminal)
 {
 	tcgetattr(STDIN_FILENO, terminal);
